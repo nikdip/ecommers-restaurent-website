9e4b8b4303640d3187ee5b32a38ab19878e3adfc
nikdip <yashdumbre10@gmail.com>
2025-09-17 14:25:17 +0530

    Fix media serving on Render and add media files

--- ps3/urls.py
+++ ps3/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:  # Only for development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
