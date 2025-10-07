6531000fda9167c8d16b4e6bb87c7776b265ed25
nikdip <yashdumbre10@gmail.com>
2025-10-07 10:46:16 +0530

    Your update message

--- ps3/urls.py
+++ ps3/urls.py
@@ -15,19 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('nikdip.urls')),
-    path('', include('paypal.standard.ipn.urls')),  # This will resolve 'paypal-ipn'
-
+    path('', include('nikdip.urls')),
+    path('paypal/', include('paypal.standard.ipn.urls')),  # ✅ better to prefix with 'paypal/'
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ✅ Serve media files only in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
